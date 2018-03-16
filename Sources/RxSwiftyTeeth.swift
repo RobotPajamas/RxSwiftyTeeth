468b551c4eb4af60128292247df86568bbb87b2e
SJ <suresh@vicarasolutions.com>
2018-03-16 18:03:48 -0400

    Copied over ViewControllers from SwiftyTeeth

---
+++ Sources/RxSwiftyTeeth.swift
@@ -0,0 +1,22 @@
+//
+//  RxSwiftyTeeth.swift
+//  SwiftyTeeth iOS
+//
+//  Created by Suresh Joshi on 2018-03-16.
+//
+
+import RxSwift
+
+public extension Reactive where Base: SwiftyTeeth {
+    func scan() -> Observable<Device> {
+        return Observable.create({ (observer) -> Disposable in
+            self.base.scan(changes: { (device) in
+                observer.onNext(device)
+            })
+            
+            return Disposables.create {
+                self.base.stopScan()
+            }
+        })
+    }
+}
