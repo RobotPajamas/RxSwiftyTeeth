8aa08106557462cf27e01ca720bd06fc48b82bc8
SJ <suresh@vicarasolutions.com>
2018-03-16 17:19:53 -0400

    Scaffolding out project (framework, unit tests, and sample app)

---
+++ RxSwiftyTeeth Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftyTeeth Sample
+//
+//  Created by Suresh Joshi on 2018-03-16.
+//  Copyright © 2018 Robot Pajamas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
