b96db706325ba2f36c7ec0fb8f005c4293416ab4
SJ <suresh@vicarasolutions.com>
2020-09-02 22:13:13 -0400

    See issue #7: Fixed RxSwift deps

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/RobotPajamas/SwiftyTeeth.git", from: "0.7.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
